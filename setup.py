872af2a95552d987a94339e7d8a579d49641b5ef
Niklas Elsbrock <mail@nelsbrock.de>
2021-08-17 20:45:52 +0200

    Versionsnummer gesetzt auf 1.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="NateMan",
-    version="1.2.0",
+    version="1.2.1",
     author="Niklas Elsbrock, Johannes Bingel",
     packages=find_packages(),
     include_package_data=True,
